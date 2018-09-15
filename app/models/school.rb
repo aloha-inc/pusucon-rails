dff80c68f78eafb3c5fe8e36693446e7ee91c421
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-15 21:12:08 +0900

    add validates of school registration page

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,11 @@
 class School < ApplicationRecord
+validates :name, :description, :image_url, :price, presence: true
+# 
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  #validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
 end
