30f20097e5b086c48f24f7b3458eec877bd4856e
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-21 16:10:42 +0900

    スクールとカートの関連付けまで

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,10 @@
 class School < ApplicationRecord
-validates :name, :description, :image_url, :price, presence: true
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
+  validates :name, :description, :image_url, :price, presence: true
 # 
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
@@ -7,5 +12,17 @@ validates :name, :description, :image_url, :price, presence: true
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   #validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, length: {minimum: 10}
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
