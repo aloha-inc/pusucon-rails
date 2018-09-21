087df37dd037132e8cc77149cabae38d09f9a4c1
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-21 15:48:12 +0900

    scaffold cart、migrateまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   root 'searches#index', as: 'searches_index'
   resources :schools
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
