36a43f849f9986f332bd031a50a07544177118c5
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-21 17:26:38 +0900

    カート内の商品名重複を解決まで

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ApplicationRecord
 	has_many :line_items, dependent: :destroy
+
+	def add_school(school)
+    current_item = line_items.find_by(school_id: school.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(school_id: school.id)
+    end
+    current_item
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_074951) do
+ActiveRecord::Schema.define(version: 2018_09_21_080119) do
 
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
