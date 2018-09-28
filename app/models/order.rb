34ac52a9d64fe2cf725647c6a6096d99605b0bf3
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-28 18:41:48 +0900

    オーダーテスト、バリデーション確認の実行（OK）

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ApplicationRecord
 	has_many :line_items, dependent: :destroy
   # ...
   validates :name, :name_kana, :email, :tel, presence: true
-  validates :schedule, inclusion: schedule.keys
+  validates :schedule, inclusion: schedules.keys
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
