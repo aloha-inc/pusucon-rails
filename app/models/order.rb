cedf1277f70a86c02ee58b168dd4e3a2d7d567cd
aloha-inc <naoharuyoshikawa@gmail.com>
2018-09-28 18:19:13 +0900

    オーダーフォームにバリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ApplicationRecord
 		"半年後以降"         => 3,
 		"相談して決めたい"    => 4
 	}
+	has_many :line_items, dependent: :destroy
+  # ...
+  validates :name, :name_kana, :email, :tel, presence: true
+  validates :schedule, inclusion: schedule.keys
 end
